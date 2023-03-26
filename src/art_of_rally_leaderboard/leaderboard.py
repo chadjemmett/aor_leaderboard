1db64b6288ad6868d3c2fe2efc01170e086e15f0
Chad Jemmett <cjem@Chads-MBP.attlocal.net>
2023-03-25 21:42:37 -0500

    Add Mocks to tests. Add board_entries for tests constants, fixed typo on stage dict

--- src/art_of_rally_leaderboard/leaderboard.py
+++ src/art_of_rally_leaderboard/leaderboard.py
@@ -9,10 +9,12 @@ class Leaderboard:
         self.default_platform = default_platform
         self.url="https://www.funselektorfun.com/artofrally/leaderboard"
 
+
     def top_ten(self, area, stage, group, direction="Forward", wx="Dry"):
         #capitalize all the args
         area = area.title()
         stage = stage.title()
+        self.check_input_cases(area, stage, group)
         direction = direction.title()
         try:
             stage = STAGES[area].get(stage) 
@@ -20,19 +22,19 @@ class Leaderboard:
             group = GROUPS[group]
         except KeyError as e:
             print("Check your capitalization or spelling. Stage example: San Benedetto. Group Example: GroupB.")
-
-
-            
         #make request and return
         url = f"{self.url}/{area}_Stage_{stage}_{direction}_{wx}_{group}/0/{self.default_platform}"
         r = requests.get(url)
         r.raise_for_status()
         return r.json()
 
+
     def my_standing(self):
         pass
 
     def full_list(self):
         pass
+
+
 
 # https://github.com/Theaninova/ArtOfRallyLeaderboardAPI/tree/master/leaderboard-connection/leaderboard-string
--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -4,7 +4,29 @@ from pytest import fixture
 import requests
 from src.art_of_rally_leaderboard.leaderboard import Leaderboard 
 import vcr
+from .board_entries import API_RESULT
 board = Leaderboard(default_platform=5)
+
+
+class MockResponse:
+    def __init__(self):
+        self.status_code = 200
+
+    @staticmethod
+    def json():
+        return API_RESULT
+
+
+    def raise_for_status(self):
+        return None 
+
+
+@fixture
+def mock_response(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+    monkeypatch.setattr(requests, 'get', mock_get)
+
 
 @fixture
 def user_keys():
@@ -20,15 +42,13 @@ def key_error_message():
                     """
 
 
-@vcr.use_cassette('tests/vcr_cassettes/leaderboard_info.yml')
-def test_top_ten(user_keys):
+def test_top_ten(user_keys, mock_response):
     top_10 = board.top_ten("Finland", "Palus", "GroupB", direction="Reverse", wx="Rain")
     assert isinstance(top_10, dict)
     assert set(user_keys).issubset(top_10['leaderboard'][0].keys())
     assert len(top_10['leaderboard']) == 10
 
-@vcr.use_cassette('tests/vcr_cassettes/leaderboard_uppercase.yml')
-def test_capitalized_word(user_keys):
+def test_capitalized_word(user_keys, mock_response):
     top_10 = board.top_ten("finland", "palus", 'Group2', "forward", "dry")
     assert isinstance(top_10, dict)
     assert top_10 != {'result': 0, 'leaderboard': []}
@@ -36,16 +56,14 @@ def test_capitalized_word(user_keys):
     assert len(top_10['leaderboard']) == 10
 
 
-@vcr.use_cassette('tests/vcr_cassettes/leaderboard_status_check.yml')
-def test_wrong_status(user_keys):
-    with pytest.raises(Exception) as e:
-        r = requests.get("https://www.funselektorfun.com/artofralllllly/leaderboard/Finland_Stage_1_Forward_Wet_60s")
-        assert e.type == HTTPError
+# def test_wrong_argument_value(capfd, mock_response):
+#     top_10 = board.top_ten("Fonland", "palussss", "Group44","forward", "dry")
+#     captured = capfd.readouterr()
+#     assert captured.out == "Check your capitalization or spelling. Stage example: San Benedetto. Group Example: GroupB.\n"
 
-@vcr.use_cassette('tests/vcr_cassettes/leaderboard_key_error.yml')
-def test_wrong_argument_value(capfd):
-    top_10 = board.top_ten("Fonland", "palussss", "Group44","forward", "dry")
-    assert top_10 == {'result': 0, 'leaderboard': []}
-    captured = capfd.readouterr()
-    assert captured.out == "Check your capitalization or spelling. Stage example: San Benedetto. Group Example: GroupB.\n"
+
+
+
+
+
 
